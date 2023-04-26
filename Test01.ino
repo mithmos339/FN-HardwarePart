342427a273f24d3e7b6a88c8ef4665932e2e5067
mithmos339 <61343336+mithmos339@users.noreply.github.com>
2023-04-26 20:38:25 +0700

    Update all one board

--- Test01.ino
+++ Test01.ino
@@ -9,13 +9,18 @@
 //#define WIFI_SSID "TOT_2.4G"
 //#define WIFI_PASSWORD "0834490533"
 
+FirebaseData data;
 int laserP = D7;
 int laserM = D8;
 int r = D2;
 int g = D1;
 const int buzzer = D5;
 const int button = D0;
-FirebaseData data;
+int plus = 0;
+int miss = 0;
+int x_status;
+int y_end;
+int t;
 
 void setup() {
   Serial.begin(9600);
@@ -38,54 +43,61 @@ void connectWiFi(){
   }
 }
 
-int plus = 0;
-int miss = 0;
-int x;
-
-void loop() {
-  if(Firebase.getInt(data,"username/plus")){//รอ SET ค่า end เป็น INT
+void getRealtime(){
+  if(Firebase.getInt(data,"username/end")){
     if(data.dataType() == "int"){
-        x = data.intData();
-    }
-    if (x == 1){
-      for(int t = 28; t >=0; t--){
-        //delay(500); //ลองลดดู
-        digitalWrite(buzzer,HIGH);
-        int value = digitalRead(laserP);
-        if (value == 0){
-          digitalWrite(r,HIGH);
-          digitalWrite(g,LOW);
-          plus = plus + 1;
-          digitalWrite(buzzer,HIGH);
-        }else{
-          digitalWrite(r,LOW);
-          digitalWrite(g,HIGH);
-          digitalWrite(buzzer,HIGH);
-        }
-        int value1 = digitalRead(laserM);
-        if (value1 == 1){
-          miss = miss + 1;
-          delay(1000);
-          //digitalWrite(buzzer,LOW);
-          //delay(10);
-          //digitalWrite(buzzer,LOW);
-          //delay(10);
-        }
-        if(t == 10){
-          digitalWrite(buzzer,LOW);
-          delay(1000);
-        }
-        if(t == 1){
-          Firebase.setBool(data,"username/status",true);
-          miss = 0;
-          plus = 0;
-          digitalWrite(buzzer,LOW);
-          delay(3000);
-        }
-        Firebase.setInt(data,"username/plus", plus);
-        Firebase.setInt(data,"username/miss", miss);
-        Firebase.setInt(data,"username/time", t);
-      }
+        y_end = data.intData();
     }
   }
+  if(Firebase.getInt(data,"username/time")){
+    if(data.dataType() == "int"){
+        t = data.intData();
+    }
+  }
+  if(Firebase.getInt(data,"username/status")){
+    if(data.dataType() == "int"){
+        x_status = data.intData();
+    }
+  }
+}
+
+void loop() {
+  getRealtime();
+  if (x_status == 1){
+    digitalWrite(buzzer,HIGH);
+    int value = digitalRead(laserP);
+    if (value == 0){
+      plus++;
+      digitalWrite(r,HIGH);
+      digitalWrite(g,LOW);
+      digitalWrite(buzzer,HIGH);
+    }else{
+      digitalWrite(r,LOW);
+      digitalWrite(g,HIGH);
+      digitalWrite(buzzer,HIGH);
+    }
+    int value1 = digitalRead(laserM);
+    if (value1 == 1){
+      miss++;
+      digitalWrite(buzzer,LOW);
+      delay(10);
+      digitalWrite(buzzer,LOW);
+      delay(10);
+    }
+    if(t == 90){
+      digitalWrite(buzzer,LOW);
+      delay(1000);
+    }
+    if(t == 0 || x_status == 0){
+      Firebase.setInt(data,"username/end",1);
+      digitalWrite(buzzer,LOW);
+      delay(1000);
+    }
+  }
+  if(t == 0 || x_status == 0){
+    miss = 0;
+    plus = 0;
+  }
+  Firebase.setInt(data,"username/plus", plus);
+  Firebase.setInt(data,"username/miss", miss);
 }
